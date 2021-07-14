733e7a96405d261faccc38985d0c25bfefd4b7e2
moustafa-7 <moustafasleem777@gmail.com>
2021-07-14 18:04:13 +0200

    test

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Welcome, my name is Mislav and this is my Flask Web App! v1'
 
-app.run(host='0.0.0.0', port=5000)
+@app.route("/")
+def index():
+    return "Welcome, my name is Moustafa Elsayed and this is my app"
+
+
+app.run(host="0.0.0.0", port=8080)
